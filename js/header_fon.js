30b6fc822c21587d7e7b4f5000f04ad8c31288e6
SRamzanA <ramzansmajov14@gmail.com>
2025-11-12 20:28:22 +0700

    Project Finished

---
+++ js/header_fon.js
@@ -0,0 +1,11 @@
+VANTA.RINGS({
+  el: "#header-vanta",
+  mouseControls: false,
+  touchControls: false,
+  gyroControls: false,
+  minHeight: 150.00,
+  minWidth: 150.00,
+  scale: 0.5,
+  scaleMobile: 1.00,
+  speed: 0.5
+})
---
+++ js/smoothscroll.js
@@ -0,0 +1,13 @@
+SmoothScroll({
+    animationTime: 800, // Время скролла 800 = 0.8 секунды
+    stepSize: 75, // Размер шага в пикселях
+
+    // Дополнительные настройки
+
+    accelerationDelta: 30, // Ускорение
+    accelerationMax: 2, // Максимальное ускорение
+    keyboardSupport: true, // Поддержка клавиатуры 
+    arrowScroll: 50, // Шаг скролла на клавиатуре в пикселях
+
+    touchpadSupport: true // Поддержка тачпада
+})
